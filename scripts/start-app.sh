aa563ddfcfe4a2f46e43fbcf5c95bd14fa682ec5
Chakraborty <debjit.chakraborty@accenture.com>
2024-11-06 20:18:48 +0530

    fix permission

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -3,8 +3,7 @@ old_tag="v1.0"
 new_tag="v1.0"
 old_image="568245924140.dkr.ecr.us-west-2.amazonaws.com/devops-repo-dc:${old_tag}"
 new_image="568245924140.dkr.ecr.us-west-2.amazonaws.com/devops-repo-dc:${new_tag}"
-sudo -i
-docker_image=$(docker ps | grep -i ${old_image})
+docker_image=$(sudo docker ps | grep -i ${old_image})
 if [[ $? != 0 ]]
 then
 docker run -d -p 80:80 $old_image
