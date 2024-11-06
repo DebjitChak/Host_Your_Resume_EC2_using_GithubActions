c9dff7f8553f5b6c09c0b51a8b576ca5bae07989
Chakraborty <debjit.chakraborty@accenture.com>
2024-11-06 20:12:05 +0530

    Changed start-app.sh script to handle container updates

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-tag="v1.0"
+old_tag="v1.0"
+new_tag=""
+old_image="568245924140.dkr.ecr.us-west-2.amazonaws.com/devops-repo-dc:${old_tag}"
+new_image="568245924140.dkr.ecr.us-west-2.amazonaws.com/devops-repo-dc:${new_tag}"
 sudo -i
-docker run -d -p 80:80 568245924140.dkr.ecr.us-west-2.amazonaws.com/devops-repo-dc:${tag}
+docker_image=$(docker ps | grep -i ${old_image})
+if [[ $? != 0 ]]
+then
+docker run -d -p 80:80 $old_image
+else
+docker stop $docker_image
+docker rm $(docker ps -qa)
+docker run -d -p 80:80 $new_image
+fi
